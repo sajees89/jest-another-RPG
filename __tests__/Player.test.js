a03a5d5265f222f244eae86e8e2a1596cc93de6e
Sajees XPS <sajees.jeya@gmail.com>
2022-01-20 16:50:57 -0500

    tests and functions

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -1,13 +1,5 @@
 const Player = require('../lib/Player'); 
-
-test("gets player's stats as an object", () => {
-    const player = new Player('Dave');
-  
-    expect(player.getStats()).toHaveProperty('potions');
-    expect(player.getStats()).toHaveProperty('health');
-    expect(player.getStats()).toHaveProperty('strength');
-    expect(player.getStats()).toHaveProperty('agility');
-  });
+const Potion = require('../lib/Potion.js');
 
   test("gets player's stats as an object", () => {
     const player = new Player('Dave');
@@ -27,3 +19,33 @@ test("gets player's stats as an object", () => {
 
       expect(player.getInventory()).toEqual(false); 
   });  
+
+  test("gets player's health value", () => {
+      const player = new Player('Dave'); 
+
+      expect(player.getHealth()).toEqual(expect.stringContaining(player.health.toString())); 
+  }); 
+
+  test('checks if player is alive or not', () => {
+      const player = new Player('Dave'); 
+
+      expect(player.isAlive()).toBeTruthy(); 
+
+      player.health = 0; 
+
+      expect(player.isAlive()).toBeFalsy(); 
+  }); 
+
+  test("subtracts from player's health", () => {
+      const player = new Player('Dave'); 
+      const oldHealth = player.health; 
+
+      player.reduceHealth(5); 
+
+      expect(player.health).toBe(oldHealth - 5); 
+
+      player.reduceHealth(999999); 
+
+      expect(player.health).toBe(0); 
+  }); 
+
